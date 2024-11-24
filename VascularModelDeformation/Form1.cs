01e8b7f5d49652747204ee8412937e783aff387e
HP <onouedod@gmail.com>
2024-11-24 20:19:51 +0900

    add

--- VascularModelDeformation/Form1.cs
+++ VascularModelDeformation/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,38 @@ namespace VascularModelDeformation
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // makemesh.py のパス (適切に変更してください!)
+            string pythonScriptPath = @"C:\git\VMD\makemesh.py";
+
+            // Python.exe のパス (適切に変更してください!)
+            string pythonExePath = @"C:\git\VMD\venv\Scripts\python.exe";
+
+            // Pythonスクリプトを実行するためのプロセス設定
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = pythonExePath,
+                Arguments = $"\"{pythonScriptPath}\"", // スクリプトの引数 (必要に応じて追加)
+                CreateNoWindow = true, // コマンドプロンプトのウィンドウを表示しない
+                UseShellExecute = false, // 標準出力を利用するため
+            };
+
+            try
+            {
+                // プロセスを開始してPythonスクリプトを実行
+                Process process = Process.Start(startInfo);
+                process.WaitForExit(); // スクリプトの実行が完了するのを待つ
+
+                MessageBox.Show("Making mesh has been done successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error executing Python script: {ex.Message}");
+            }
+
         }
     }
 }
