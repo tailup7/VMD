f89a3c89524ad4d3ac5d9b4f06c1f49ab3b79b35
HP <onouedod@gmail.com>
2024-11-29 23:45:22 +0900

    add

--- VascularModelDeformation/Node.cs
+++ VascularModelDeformation/Node.cs
@@ -108,5 +108,14 @@ namespace VascularModelDeformation
             this.Z = z;
         }
 
+        // cf. https://www.mum-meblog.com/entry/tyr-utility/csharp-hashset 
+        public override int GetHashCode()  
+        {                                       
+            int hash = 17;
+            hash = hash * 23 + X.GetHashCode();
+            hash = hash * 23 + Y.GetHashCode();
+            hash = hash * 23 + Z.GetHashCode();
+            return hash;
+        }
     }
 }
